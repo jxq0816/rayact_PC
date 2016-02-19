9b61d997f751ac721bb3d367d0cadb890dade3a9
dell <1131385708@qq.com>
2016-02-19 12:53:30 +0800

    12

---
+++ api/src/main/java/com/bra/modules/cms/service/ArticleService.java
@@ -0,0 +1,22 @@
+package com.bra.modules.cms.service;
+import com.bra.common.service.CrudService;
+import com.bra.modules.cms.dao.ArticleDao;
+import com.bra.modules.cms.eneity.Article;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by xiaobin on 16/2/17.
+ */
+@Service
+@Transactional(readOnly = true)
+public class ArticleService extends CrudService<ArticleDao,Article> {
+
+    @Transactional(readOnly = false)
+    public List<Article> homeArticle(Article article) {
+        return dao.homeArticle(article);
+    }
+
+}
