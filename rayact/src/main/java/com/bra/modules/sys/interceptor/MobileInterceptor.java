d5adbb40fc981ee48a08f121aa767cd3292a3e62
DDT <1096199259@qq.com>
2016-04-06 12:53:32 +0800

    移动端跳转禁止

--- rayact/src/main/java/com/bra/modules/sys/interceptor/MobileInterceptor.java
+++ rayact/src/main/java/com/bra/modules/sys/interceptor/MobileInterceptor.java
@@ -3,15 +3,12 @@
  */
 package com.bra.modules.sys.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.bra.common.service.BaseService;
-import com.bra.common.utils.UserAgentUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.bra.common.utils.StringUtils;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 手机端视图拦截器
@@ -29,12 +26,12 @@ public class MobileInterceptor extends BaseService implements HandlerInterceptor
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
 			ModelAndView modelAndView) throws Exception {
-		if (modelAndView != null){
-			// 如果是手机或平板访问的话，则跳转到手机视图页面。
-			if(UserAgentUtils.isMobileOrTablet(request) && !StringUtils.startsWithIgnoreCase(modelAndView.getViewName(), "redirect:")){
-				modelAndView.setViewName("mobile/" + modelAndView.getViewName());
-			}
-		}
+//		if (modelAndView != null){
+//			// 如果是手机或平板访问的话，则跳转到手机视图页面。
+//			if(UserAgentUtils.isMobileOrTablet(request) && !StringUtils.startsWithIgnoreCase(modelAndView.getViewName(), "redirect:")){
+//				modelAndView.setViewName("mobile/" + modelAndView.getViewName());
+//			}
+//		}
 	}
 
 	@Override
