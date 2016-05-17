5b2331d030455f149dfd8681adbf31d2940e9ac2
jiangxingqi <jxq0816@126.com>
2016-05-17 12:51:07 +0800

    场馆列表 接口

--- rayact/src/main/java/com/bra/modules/reserve/service/ReserveVenueService.java
+++ rayact/src/main/java/com/bra/modules/reserve/service/ReserveVenueService.java
@@ -12,6 +12,8 @@ import com.bra.modules.reserve.utils.AuthorityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.*;
 
@@ -40,16 +42,33 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
         }
         return super.findList(reserveVenue);
     }
+
     public List<Map> findListForApp(ReserveVenue reserveVenue) {
         List<Map> venueList = dao.findListForApp(reserveVenue);
-        for(Map venueMap: venueList){
-            String venueId=(String)venueMap.get("id");
-            ReserveVenue venue=new ReserveVenue();
+        for (Map venueMap : venueList) {
+            String venueId = (String) venueMap.get("venueId");
+            ReserveVenue venue = new ReserveVenue();
             venue.setId(venueId);
             List<Map> imgList = dao.findImgPathList(venue);
-            venueMap.put("imgList",imgList);
+            if(imgList!=null){
+                Map img=imgList.get(0);
+                String imgId=(String)img.get("imgId");
+                WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+                String pre="http://192.168.1.173:8080/rayact/mechanism/file/image/";
+                String url=pre+imgId;
+                venueMap.put("imgUrl", url);
+            }
         }
         return venueList;
+    }
+
+    public List<Map> findImgList(ReserveVenue reserveVenue) {
+
+        String venueId = reserveVenue.getId();
+        ReserveVenue venue = new ReserveVenue();
+        venue.setId(venueId);
+        List<Map> imgList = dao.findImgPathList(venue);
+        return imgList;
     }
 
     public Page<ReserveVenue> findPage(Page<ReserveVenue> page, ReserveVenue reserveVenue) {
@@ -73,19 +92,19 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
     }
 
     //日报表
-    public List<ReserveVenueProjectFieldDayReport> dayReport(ReserveVenueProjectFieldIntervalReport fieldReport){
+    public List<ReserveVenueProjectFieldDayReport> dayReport(ReserveVenueProjectFieldIntervalReport fieldReport) {
 
-        ReserveVenue venue=fieldReport.getReserveVenue();
-        ReserveField field=fieldReport.getReserveField();
-        ReserveProject project=fieldReport.getReserveProject();
+        ReserveVenue venue = fieldReport.getReserveVenue();
+        ReserveField field = fieldReport.getReserveField();
+        ReserveProject project = fieldReport.getReserveProject();
 
-        ReserveVenueProjectFieldDayReport dayReport=new ReserveVenueProjectFieldDayReport();
+        ReserveVenueProjectFieldDayReport dayReport = new ReserveVenueProjectFieldDayReport();
         dayReport.setReserveVenue(venue);
         dayReport.setReserveProject(project);
         dayReport.setReserveField(field);
 
-        Date startDate=fieldReport.getStartDate();
-        Date endDate=fieldReport.getEndDate();
+        Date startDate = fieldReport.getStartDate();
+        Date endDate = fieldReport.getEndDate();
 
         Calendar startCal = Calendar.getInstance();
         startCal.setTime(startDate);
@@ -96,9 +115,9 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
 
         List<ReserveVenueProjectFieldDayReport> list = new ArrayList<>();
 
-        while(endCal.after(startCal)|| endCal.equals(startCal)){
+        while (endCal.after(startCal) || endCal.equals(startCal)) {
 
-            Date day =startCal.getTime();
+            Date day = startCal.getTime();
             dayReport.setDay(day);
             List<ReserveVenueProjectFieldDayReport> dayReportList = dao.dayReport(dayReport);
             if (dayReportList == null) {
@@ -115,60 +134,62 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
         }
         return list;
     }
+
     /* 场地散客收入*/
-    public List<ReserveVenueProjectIntervalReport> reserveVenueProjectDividedIntervalReport(ReserveVenueProjectIntervalReport reserveVenueProjectIntervalReport){
+    public List<ReserveVenueProjectIntervalReport> reserveVenueProjectDividedIntervalReport(ReserveVenueProjectIntervalReport reserveVenueProjectIntervalReport) {
         List<ReserveVenueProjectIntervalReport> venueProjectDividedReports = dao.reserveVenueProjectDividedIntervalReport(reserveVenueProjectIntervalReport);//场馆 项目 散客 收入统计
         return venueProjectDividedReports;
     }
+
     //场馆收入统计
     public ReserveVenueIncomeIntervalReport reserveVenueIncomeIntervalReport(ReserveVenueProjectIntervalReport venueProjectReport) {
-        Double billSum=0.0;
-        Double storedCardSum=0.0;
-        Double cashSum=0.0;
-        Double bankCardSum=0.0;
-        Double weiXinSum=0.0;
-        Double aliPaySum=0.0;
-        Double otherSum=0.0;
-        Double dueSum=0.0;
-        List<ReserveVenueProjectIntervalReport>  venueProjectList=dao.findVenueProjectList(venueProjectReport);//查询场馆下的所有场地
+        Double billSum = 0.0;
+        Double storedCardSum = 0.0;
+        Double cashSum = 0.0;
+        Double bankCardSum = 0.0;
+        Double weiXinSum = 0.0;
+        Double aliPaySum = 0.0;
+        Double otherSum = 0.0;
+        Double dueSum = 0.0;
+        List<ReserveVenueProjectIntervalReport> venueProjectList = dao.findVenueProjectList(venueProjectReport);//查询场馆下的所有场地
 
         List<ReserveVenueProjectIntervalReport> venueProjectBlockReports = dao.reserveVenueProjectBlockIntervalReport(venueProjectReport);//场馆 项目 包场 收入统计
         List<ReserveVenueProjectIntervalReport> venueProjectDividedReports = dao.reserveVenueProjectDividedIntervalReport(venueProjectReport);//场馆 项目 散客 收入统计
-        for(ReserveVenueProjectIntervalReport i:venueProjectList){// 遍历 场馆和项目
+        for (ReserveVenueProjectIntervalReport i : venueProjectList) {// 遍历 场馆和项目
             ReserveProject project = i.getReserveProject();
             ReserveVenue venue = i.getReserveVenue();
-            Double bill=0.0;
-            Double storedCard=0.0;
-            Double cash=0.0;
-            Double bankCard=0.0;
-            Double weiXin=0.0;
-            Double aliPay=0.0;
-            Double other=0.0;
-            Double due=0.0;
+            Double bill = 0.0;
+            Double storedCard = 0.0;
+            Double cash = 0.0;
+            Double bankCard = 0.0;
+            Double weiXin = 0.0;
+            Double aliPay = 0.0;
+            Double other = 0.0;
+            Double due = 0.0;
             //将包场收入加入
-            for(ReserveVenueProjectIntervalReport j:venueProjectBlockReports){
-                if(j.getReserveProject().getId().equals(project.getId())&&j.getReserveVenue().getId().equals(venue.getId())){
-                    bill+=j.getBill();
-                    storedCard+=j.getFieldBillStoredCard();
-                    cash+=j.getFieldBillCash();
-                    bankCard+=j.getFieldBillBankCard();
-                    weiXin+=j.getFieldBillWeiXin();
-                    aliPay+=j.getFieldBillAliPay();
-                    other+=j.getFieldBillOther();
-                    due+=j.getFieldBillDue();
+            for (ReserveVenueProjectIntervalReport j : venueProjectBlockReports) {
+                if (j.getReserveProject().getId().equals(project.getId()) && j.getReserveVenue().getId().equals(venue.getId())) {
+                    bill += j.getBill();
+                    storedCard += j.getFieldBillStoredCard();
+                    cash += j.getFieldBillCash();
+                    bankCard += j.getFieldBillBankCard();
+                    weiXin += j.getFieldBillWeiXin();
+                    aliPay += j.getFieldBillAliPay();
+                    other += j.getFieldBillOther();
+                    due += j.getFieldBillDue();
                 }
             }
             //将散客收入加入
-            for(ReserveVenueProjectIntervalReport k:venueProjectDividedReports){
-                if(k.getReserveProject().getId().equals(project.getId())&&k.getReserveVenue().getId().equals(venue.getId())){
-                    bill+=k.getBill();
-                    storedCard+=k.getFieldBillStoredCard();
-                    cash+=k.getFieldBillCash();
-                    bankCard+=k.getFieldBillBankCard();
-                    weiXin+=k.getFieldBillWeiXin();
-                    aliPay+=k.getFieldBillAliPay();
-                    other+=k.getFieldBillOther();
-                    due+=k.getFieldBillDue();
+            for (ReserveVenueProjectIntervalReport k : venueProjectDividedReports) {
+                if (k.getReserveProject().getId().equals(project.getId()) && k.getReserveVenue().getId().equals(venue.getId())) {
+                    bill += k.getBill();
+                    storedCard += k.getFieldBillStoredCard();
+                    cash += k.getFieldBillCash();
+                    bankCard += k.getFieldBillBankCard();
+                    weiXin += k.getFieldBillWeiXin();
+                    aliPay += k.getFieldBillAliPay();
+                    other += k.getFieldBillOther();
+                    due += k.getFieldBillDue();
                 }
             }
             i.setFieldBillStoredCard(storedCard);
@@ -180,14 +201,14 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
             i.setFieldBillOther(other);
             i.setFieldBillDue(due);
             //场地 项目总合计 计算
-            billSum+=bill;
-            storedCardSum+=storedCard;
-            cashSum+=cash;
-            bankCardSum+=bankCard;
-            weiXinSum+=weiXin;
-            aliPaySum+=aliPay;
-            otherSum+=other;
-            dueSum+=due;
+            billSum += bill;
+            storedCardSum += storedCard;
+            cashSum += cash;
+            bankCardSum += bankCard;
+            weiXinSum += weiXin;
+            aliPaySum += aliPay;
+            otherSum += other;
+            dueSum += due;
         }
         /*明细 开始*/
         for (ReserveVenueProjectIntervalReport i : venueProjectList) {//场馆 项目遍历
@@ -196,7 +217,7 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
         }
         /*明细 结束*/
         //场地 项目 总合计设置
-        ReserveVenueIncomeIntervalReport venueReport=new ReserveVenueIncomeIntervalReport();
+        ReserveVenueIncomeIntervalReport venueReport = new ReserveVenueIncomeIntervalReport();
         venueReport.setBill(billSum);
         venueReport.setStoredCardBill(storedCardSum);
         venueReport.setCashBill(cashSum);
@@ -211,7 +232,6 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
 
     /**
      * 场馆 项目 场地 收入统计
-     *
      */
     public List<ReserveVenueProjectFieldIntervalReport> reserveVenueProjectFieldIntervalReport(ReserveVenueProjectIntervalReport projectIntervalReport) {
         if (projectIntervalReport != null) {
@@ -222,9 +242,9 @@ public class ReserveVenueService extends CrudService<ReserveVenueDao, ReserveVen
         return filedReports;
     }
 
-    public List<ReserveVenueTotalIntervalReport> totalIncomeReport(ReserveVenueTotalIntervalReport intervalTotalReport){
+    public List<ReserveVenueTotalIntervalReport> totalIncomeReport(ReserveVenueTotalIntervalReport intervalTotalReport) {
 
-        List<ReserveVenueTotalIntervalReport> list=dao.totalIncomeReport(intervalTotalReport);
+        List<ReserveVenueTotalIntervalReport> list = dao.totalIncomeReport(intervalTotalReport);
         return list;
     }
 }
