6ac8d8a8a95fd0cfb2ad055399e6d30d6912dc61
jiangxingqi <jxq0816@126.com>
2016-06-09 12:16:12 +0800

    定时任务

---
+++ rayact/src/main/java/com/bra/modules/task/OrderTaskManager.java
@@ -0,0 +1,10 @@
+package com.bra.modules.task;
+
+/**
+ * Created by lenovo on 2016/6/9.
+ */
+public class OrderTaskManager {
+    public void doSomethingWithDelay() {
+        System.out.println("定时任务开始执行……");
+    }
+}
