f443569b349f4c14de087f7d048c93c8a631eac2
jiangxingqi <jxq0816@126.com>
2016-06-22 17:53:49 +0800

    ping++ webhooks

--- rayact/src/main/java/com/bra/modules/reserve/web/ReserveAppController.java
+++ rayact/src/main/java/com/bra/modules/reserve/web/ReserveAppController.java
@@ -13,6 +13,8 @@ import com.bra.modules.reserve.service.ReserveVenueConsItemService;
 import com.bra.modules.reserve.service.ReserveVenueService;
 import com.bra.modules.reserve.utils.TimeUtils;
 import com.bra.modules.util.pingplusplus.PingPlusPlusService;
+import com.pingplusplus.model.Charge;
+import com.pingplusplus.model.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -139,7 +141,7 @@ public class ReserveAppController extends BaseController {
         return "reserve/saleField/reserveAppField";
     }
     /**
-     * 结算订单
+     * ping++结算订单
      *
      * @param
      * @return
@@ -149,6 +151,33 @@ public class ReserveAppController extends BaseController {
     public String charge(String order_no, int amount,String subject,String body,String channel, String client_ip) {
         String charge = pingPlusPlusService.charge(order_no, amount, subject, body, channel, client_ip);
         return charge;
+    }
+    @RequestMapping(value = "Webhooks")
+    @ResponseBody
+    public void Webhooks (Event event) {
+        if("charge.succeeded".equals(event.getType())){
+            Charge charge=(Charge)event.getData().getObject();
+            String orderId=charge.getId();
+            String channel=charge.getChannel();
+            String payType=null;
+            int amountFen=charge.getAmount();
+            Double amountYuan= amountFen*1.0/100;//ping++扣款
+            Double weiXinInput = null;
+            Double aliPayInput = null;
+
+            if("wx".equals(channel)){
+                payType="4";//支付类型(1:储值卡，2:现金,3:银行卡,4:微信,5:支付宝,6:优惠券，7：打白条;8:多方式付款;9:微信个人，10：支付宝（个人）)
+                weiXinInput=amountYuan;
+            }else if("alipay".equals(channel)){
+                payType="5";
+                aliPayInput=amountYuan;
+            }
+            Double couponInput;
+            ReserveVenueCons reserveVenueCons = reserveAppVenueConsService.get(orderId);
+            Double orderPrice=reserveVenueCons.getOrderPrice();
+            couponInput=orderPrice-amountYuan;//订单金额-ping++扣款 等于优惠金额
+            saveSettlement(orderId,payType,amountYuan,0.0,0.0,weiXinInput,aliPayInput,couponInput);
+        }
     }
 
     /**
